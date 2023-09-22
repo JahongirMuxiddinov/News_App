537d87e69157d35a02008bb4ab3ccab2bba4129e
Urinov Farrukh <UrinovFarrukh27@icloud.com>
2023-09-22 15:23:52 +0500

    Initial Commit

---
+++ News_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News_App
+//
+//  Created by User on 22/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
